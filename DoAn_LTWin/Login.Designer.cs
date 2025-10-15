0431f7ad6a9ac77325ce40b48bcf6bd0d9b0c6e3
Danh Nguyeen <Danhn9472@gmail.com>
2025-10-15 22:12:53 +0700

    Login Update

--- DoAn_LTWin/Login.Designer.cs
+++ DoAn_LTWin/Login.Designer.cs
@@ -28,29 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(310, 94);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(491, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 37);
+            this.label1.Size = new System.Drawing.Size(190, 45);
             this.label1.TabIndex = 0;
             this.label1.Text = "Đăng Nhập";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(302, 295);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtPass.Location = new System.Drawing.Point(413, 205);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(327, 49);
+            this.txtPass.TabIndex = 2;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(413, 129);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(327, 49);
+            this.txtUser.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Black;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLogin.Location = new System.Drawing.Point(499, 282);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(165, 51);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Đăng Nhập";
+            this.btnLogin.UseVisualStyleBackColor = false;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Silver;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Name = "Login";
             this.Text = "Log In ";
             this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +112,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- DoAn_LTWin/Login.cs
+++ DoAn_LTWin/Login.cs
@@ -16,30 +16,36 @@ namespace DoAn_LTWin
         public Login()
         {
             InitializeComponent();
-            LoadCustomFont();
+
         }
-        Dictionary<string, FontFamily> fontMap = new Dictionary<string, FontFamily>();
-        private void LoadCustomFont()
-        {
-            // Đường dẫn đến file font (ví dụ trong thư mục project)
-            string[] fontPath =
-            {
-                Application.StartupPath + @"\font\MTO Astro City.ttf"
-            };
-            PrivateFontCollection privateFonts = new PrivateFontCollection();   
-            // Duyệt qua từng font và thêm vào bộ sưu tập
-            foreach (var file in fontPath)
-            {
-                privateFonts.AddFontFile(file);
-                var family = privateFonts.Families[privateFonts.Families.Length - 1]; // lấy font vừa thêm cuối cùng
-                fontMap[family.Name] = family;
-            }
-            var actualKey = fontMap.Keys.First();
-            label1.Font = new Font(fontMap[actualKey], 25);
-        }
+        
         private void Login_Load(object sender, EventArgs e)
         {
+            SetPlaceholder(txtUser, "Nhập tên đăng nhập:");
+            SetPlaceholder(txtPass, "Nhập mật khẩu:");
+        }
+        private void SetPlaceholder(TextBox tb, string placeholder)
+        {
+            tb.Text = placeholder;
+            tb.ForeColor = Color.Gray;
+            tb.Font = new Font(tb.Font.FontFamily, 20);
+            tb.Enter += (s, e) =>
+            {
+                if (tb.Text == placeholder)
+                {
+                    tb.Text = "";
+                    tb.ForeColor = Color.Black;
+                }
+            };
 
+            tb.Leave += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(tb.Text))
+                {
+                    tb.Text = placeholder;
+                    tb.ForeColor = Color.Gray;
+                }
+            };
         }
     }
 }
