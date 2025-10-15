d18b446580845a472b1e49ae35832d844fddd89e
Bi_Local <Danhn9472@gmail.com>
2025-10-15 19:43:33 +0700

    Add project files.

---
+++ DoAn_LTWin/Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace DoAn_LTWin
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            LoadCustomFont();
+        }
+        Dictionary<string, FontFamily> fontMap = new Dictionary<string, FontFamily>();
+        private void LoadCustomFont()
+        {
+            // Đường dẫn đến file font (ví dụ trong thư mục project)
+            string[] fontPath =
+            {
+                Application.StartupPath + @"\font\MTO Astro City.ttf"
+            };
+            PrivateFontCollection privateFonts = new PrivateFontCollection();   
+            // Duyệt qua từng font và thêm vào bộ sưu tập
+            foreach (var file in fontPath)
+            {
+                privateFonts.AddFontFile(file);
+                var family = privateFonts.Families[privateFonts.Families.Length - 1]; // lấy font vừa thêm cuối cùng
+                fontMap[family.Name] = family;
+            }
+            var actualKey = fontMap.Keys.First();
+            label1.Font = new Font(fontMap[actualKey], 25);
+        }
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DoAn_LTWin/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DoAn_LTWin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoAn_LTWin")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d23eef9-8ea1-49d9-bd3d-312f40f5f675")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
